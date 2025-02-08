4d1969efa3ecd89181090d212f713cecb0eff357
musseGkel <musseg437@gmail.com>
2025-02-08 18:42:52 +0100

    fix: pass the required param to Ownable Constructor

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract MyNFT is ERC721URIStorage, Ownable {
     uint256 private _tokenIds;
 
-    constructor() ERC721("MyNFTCollection", "MYNFT") {}
+    constructor() ERC721("MyNFTCollection", "MYNFT") Ownable(msg.sender) {}
 
     function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
         _tokenIds++;
